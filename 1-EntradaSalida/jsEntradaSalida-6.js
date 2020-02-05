286b23c0224e6e628ba502d2fb3fd9f7ecc3a6e0
CofranVitae <FranSagn@gmail.com>
2020-02-05 12:23:30 -0300

    asdasdj

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,13 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+var numero1
+var numero2
+numero1=document.getElementById("numeroUno").Value;
+numero2=document.getElementById("numeroDos").value;
 
+
+
+alert()
 }
 
