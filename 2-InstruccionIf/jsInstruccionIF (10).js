a2f74ce8b9bd474abe0f283c211797b0934af4ce
CofranVitae <Fransagn@gmail.com>
2020-02-08 17:31:09 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,24 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var num;
 	
+	num=Math.floor((Math.random()*10))
+	
+    alert(num);
+	
+	if(num>=9)
+	{
+		alert("Excelente");
+	}
+	else if(num>=4 && num<=8)
+	{
+		alert("Aprobado");
+	}
+	else
+	{
+		alert("Vamos, la proxima se puede");
+	}
+
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,7 +2,7 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	var num;
-	num=Math.floor((Math.random()*((10-1))+1)
+	num=Math.floor((Math.random()*10))
 	alert(num)
 
 }//FIN DE LA FUNCIÓN
