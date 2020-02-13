a07d95ea817b570f35387cb2aa03cbf0ab94c6ea
CofranVitae <FranSagn@gmail.com>
2020-02-13 19:53:50 -0300

    hice el 9 papa

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,24 +1,31 @@
 function mostrar()
-{
 
-	var contador=0;
+{
 	var numero;
-	var Mayor;
-	var Menor;
-	// declarar variables
-	
+	var maximo=0;
+	var minimo;
 	var respuesta="si";
 	
-	
+	while(respuesta!="no")
+	{
+		numero=prompt("Escribí un número");
+		respuesta=prompt("Si no queres singresar mas numeros, escribi no, si no escribi cualquier cosa.");
+		numero=parseInt(numero);
 		
 
+		if(numero>maximo)
+		{
+			maximo=numero;
+		}
+		else if(numero<minimo || numero<maximo)
+		{
+			minimo=numero;
+		}
 		
+	}	
 
-
-	
-	}
-
-
+	document.getElementById("maximo").value=maximo;
+	document.getElementById("minimo").value=minimo;
 
 
 }//FIN DE LA FUNCIÓN
